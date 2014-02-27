8f7887364c4ffbe611e890d08cd7ed0ec83caaad
Habitats <mail@habitats.no>
2014-02-27 18:15:06 +0100

    format

--- src/crawlie/Crawlie.java
+++ src/crawlie/Crawlie.java
@@ -3,72 +3,73 @@ package crawlie;
 import java.util.ArrayList;
 
 public class Crawlie {
-	private AnalyzedPages analyzedPages;
-	private final DiscoveredQueue discoveredPages;
+  private AnalyzedPages analyzedPages;
+  private final DiscoveredQueue discoveredPages;
 
-	private long start;
-	private final ArrayList<Crawler> crawlers;
-	private final DatabaseController db;
+  private long start;
+  private final ArrayList<Crawler> crawlers;
+  private final DatabaseController db;
 
-	public Crawlie() {
-		analyzedPages = new AnalyzedPages();
-		discoveredPages = new DiscoveredQueue();
-		crawlers = new ArrayList<Crawler>();
-		db = new DatabaseController();
-	}
+  public Crawlie() {
+    analyzedPages = new AnalyzedPages();
+    discoveredPages = new DiscoveredQueue();
+    crawlers = new ArrayList<Crawler>();
+    db = new DatabaseController();
+  }
 
-	private void init() {
-		start = System.currentTimeMillis();
+  private void init() {
+    start = System.currentTimeMillis();
 
-		DiscoveredPage seedPage = new DiscoveredPage(Config.getInstance().getSeed(), "seed");
+    DiscoveredPage seedPage = new DiscoveredPage(Config.getInstance().getSeed(), "seed");
 
-		startWorkers();
-		discoveredPages.add(seedPage);
-	}
+    startWorkers();
+    discoveredPages.add(seedPage);
+  }
 
-	private void startWorkers() {
-		for (int i = 0; i < Config.getInstance().getMaxWorkers(); i++) {
-			Crawler crawler = new Crawler(this);
-			Thread crawlerThread = new Thread(crawler);
-			crawlerThread.start();
-		}
-	}
+  private void startWorkers() {
+    for (int i = 0; i < Config.getInstance().getMaxWorkers(); i++) {
+      Crawler crawler = new Crawler(this);
+      Thread crawlerThread = new Thread(crawler);
+      crawlerThread.start();
+    }
+  }
 
-	public static void main(String[] args) {
-		Crawlie crawlie = new Crawlie();
-		crawlie.init();
-	}
+  public static void main(String[] args) {
+    Crawlie crawlie = new Crawlie();
+    crawlie.init();
+  }
 
-	public AnalyzedPages getAnalyzedPages() {
-		return analyzedPages;
-	}
+  public AnalyzedPages getAnalyzedPages() {
+    return analyzedPages;
+  }
 
-	public DiscoveredQueue getDiscoveredPages() {
-		return discoveredPages;
-	}
+  public DiscoveredQueue getDiscoveredPages() {
+    return discoveredPages;
+  }
 
-	// this gets called when all workers in a batch are done
-	public synchronized void analyzeBatch() {
-		if (!workersDone())
-			return;
-		db.addPages(analyzedPages);
-		if (analyzedPages.size() < Config.getInstance().getMaxPages()) {
-			analyzedPages.dumpPages();
-			startWorkers();
-		}
-		Logger.log("Analyzed " + analyzedPages.size() + " pages, in " + (System.currentTimeMillis() - start) / 1000 + " seconds.");
+  // this gets called when all workers in a batch are done
+  public synchronized void analyzeBatch() {
+    if (!workersDone())
+      return;
+    db.addPages(analyzedPages);
+    if (analyzedPages.size() < Config.getInstance().getMaxPages()) {
+      analyzedPages.dumpPages();
+      startWorkers();
+    }
+    Logger.log("Analyzed " + analyzedPages.size() + " pages, in "
+        + (System.currentTimeMillis() - start) / 1000 + " seconds.");
 
-	}
+  }
 
-	private boolean workersDone() {
-		return crawlers.isEmpty();
-	}
+  private boolean workersDone() {
+    return crawlers.isEmpty();
+  }
 
-	public synchronized void addWorker(Crawler crawler) {
-		crawlers.add(crawler);
-	}
+  public synchronized void addWorker(Crawler crawler) {
+    crawlers.add(crawler);
+  }
 
-	public synchronized void removeWorker(Crawler crawler) {
-		crawlers.remove(crawler);
-	}
+  public synchronized void removeWorker(Crawler crawler) {
+    crawlers.remove(crawler);
+  }
 }
